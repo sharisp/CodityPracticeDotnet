89bb38809be87fdac287433139340868251886e1
Andrew Wang <wthsir@gmail.com>
2025-09-04 11:38:14 +1000

    init

---
+++ Codity/BinaryGap.cs
@@ -0,0 +1,16 @@
+﻿namespace Codity
+{
+    public class BinaryGap
+    {
+        /*
+         1. BinaryGap
+Problem:
+A binary gap within a positive integer N is any maximal sequence of consecutive zeros surrounded by ones in the binary representation of N.
+Write a function that returns the length of the longest binary gap of N. If N doesn’t contain a binary gap, return 0.
+Example:
+N = 529 → binary 1000010001 → longest gap = 4
+        */
+
+
+    }
+}

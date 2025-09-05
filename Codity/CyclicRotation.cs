519650c214a8a2c44cf39c2ad07135758b276f6f
Andrew Wang <wthsir@gmail.com>
2025-09-05 11:45:25 +1000

    feat: cyclicrotation

---
+++ Codity/CyclicRotation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Codity
+{
+    public class CyclicRotation
+    {
+        /* Problem:
+ An array A consisting of N integers is given.Rotation of the array means each element is shifted right by one index, and the last element is moved to the first place.
+ Write a function that rotates array A K times.
+ Example:
+     • A = [3, 8, 9, 7, 6], K = 3 → [9, 7, 6, 3, 8]
+ Solution(C#):
+        */
+        public int[] CyclicRotationFunc(int[] A, int K)
+        {
+            int[] arr = new int[A.Length];
+            int rotations = K % A.Length; // Handle cases where K is greater than the length of A
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                int pos=(i+ rotations)%A.Length;
+                arr[pos] = A[i];
+            }
+            return arr;
+        }
+    }
+}
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -11,5 +11,18 @@ namespace UnitTest
             Assert.AreEqual(1, bg.BinaryGapFunc(20));
            
         }
+
+        [TestMethod]
+        public void TestCycliRotation()
+        {
+            var cy = new Codity.CyclicRotation();
+            int[] A1 = { 3, 8, 9, 7, 6 };
+            int K1 = 3;
+            int[] expected1 = { 9, 7, 6, 3, 8 };
+            var get = cy.CyclicRotationFunc(A1, K1);
+       
+            CollectionAssert.AreEqual(expected1, get);
+
+        }
     }
 }

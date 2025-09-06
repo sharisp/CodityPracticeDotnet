4846818e17da978798ef9200222a6702072e2304
Andrew Wang <wthsir@gmail.com>
2025-09-06 10:35:32 +1000

    feat: OddOccurrencesInArray use dictionary solve

---
+++ Codity/OddOccurrencesInArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+
+namespace Codity
+{
+    public class OddOccurrencesInArray
+    {
+        /* Problem:
+ A non-empty array A consisting of N integers is given.Every element occurs an even number of times, except for one element that occurs an odd number of times.
+ Write a function that finds this element.
+ Example:
+     • A = [9, 3, 9, 3, 9, 7, 9] → 7
+ Solution (C#):*/
+
+        public int OddOccurrencesInArrayByDictionary(int[] A)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            foreach (var item in A)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict[item] = 1;
+                }
+            }
+            foreach (var kvp in dict)
+            {
+                if (kvp.Value % 2 != 0)
+                {
+                    return kvp.Key;
+                }
+            }
+            return -1; // Return -1 if no odd occurrence is found
+        }
+    }
+}
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace UnitTest
             Codity.BinaryGap bg = new Codity.BinaryGap();
             Assert.AreEqual(4, bg.BinaryGapFunc(529));
             Assert.AreEqual(1, bg.BinaryGapFunc(20));
-           
+
         }
 
         [TestMethod]
@@ -20,10 +20,10 @@ namespace UnitTest
             int K1 = 3;
             int[] expected1 = { 9, 7, 6, 3, 8 };
             var get1 = cy.CyclicRotationFunc(A1, K1);
-       
+
             CollectionAssert.AreEqual(expected1, get1);
 
-            int[] A2 = {};
+            int[] A2 = { };
             var get2 = cy.CyclicRotationFunc(A2, 5);
             CollectionAssert.AreEqual(A2, get2);
 
@@ -34,6 +34,18 @@ namespace UnitTest
 
             CollectionAssert.AreEqual(expected3, get3);
 
+        }
+
+
+        [TestMethod]
+        public void TestOddOccurrencesInArrayByDictionary()
+        {
+            var oddOccurrences = new Codity.OddOccurrencesInArray();
+            int[] A1 = { 9, 3, 9, 3, 9, 7, 9 };
+            Assert.AreEqual(7, oddOccurrences.OddOccurrencesInArrayByDictionary(A1));
+
+            int[] A2 = { 9, 3, 9, 3, 9, 7, 9, 9, 9, 9, 7 };
+            Assert.AreEqual(9, oddOccurrences.OddOccurrencesInArrayByDictionary(A2));
         }
     }
 }

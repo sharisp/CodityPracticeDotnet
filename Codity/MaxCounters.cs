c57086fdabbd017fa6d90fa46e5641dc61a2aacd
Andrew Wang <wthsir@gmail.com>
2025-09-14 22:46:43 +1000

    feat: maxcounters emputy func

---
+++ Codity/MaxCounters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Codity
+{
+    public class MaxCounters
+    {
+        /*  You have N counters, initially all set to 0. You are given an array of operations:
+      • if A[i] = X, increase counter X by 1
+      • if A[i] = N + 1, set all counters to the maximum value of any counter
+  Write a function that returns the state of the counters after all operations.
+  Solution(C#):
+        */
+        public int[] MaxCountersFunc(int N, int[] A)
+        {
+            //wait to implement
+            return A;
+        }
+    }

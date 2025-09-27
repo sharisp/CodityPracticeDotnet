fc068bbbd2d71dd82fe33f39e13c8c0796a64046
Andrew Wang <wthsir@gmail.com>
2025-09-27 20:38:44 +1000

    feat: merge sorted array

---
+++ Codity/leetcode/MergeSortedArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codity.leetcode
+{
+    internal class MergeSortedArray
+    {
+        /*
+         88. Merge Sorted Array
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+        */
+        /// <summary>
+        /// basic solution
+        /// </summary>
+        /// <param name="nums1"></param>
+        /// <param name="m"></param>
+        /// <param name="nums2"></param>
+        /// <param name="n"></param>
+        public void Merge1(int[] nums1, int m, int[] nums2, int n)
+        {
+            for (int i = m; i < m + n; i++)
+            {
+                nums1[i] = nums2[i - m];
+            }
+            Array.Sort(nums1);
+        }
+
+    }
+}
